11f8f57221b0eae3b3b0c22bb948b3e9724ba3ba
jayrave <jayanthan.raveendiran@gmail.com>
2018-08-27 00:51:03 -0700

    add more error types

--- src/reader/error.rs
+++ src/reader/error.rs
@@ -5,17 +5,19 @@ use xml::reader;
 
 #[derive(Debug)]
 pub enum Error {
-	IoError(io::Error), 
-	XmlError(reader::Error), 
-	SyntaxError(String)
+	IoError(io::Error),
+    LogicError(String),
+	SyntaxError(String),
+    XmlError(reader::Error)
 }
 
 impl error::Error for Error {
 	fn cause(&self) -> Option<&error::Error> {
 		match self {
-			Error::XmlError(error) => Some(error),
-			Error::IoError(error) => Some(error), 
-			Error::SyntaxError(message) => None
+			Error::IoError(error) => Some(error),
+            Error::LogicError(_message) => None,
+			Error::SyntaxError(_message) => None,
+			Error::XmlError(error) => Some(error)
 		}
 	}
 }
@@ -23,9 +25,10 @@ impl error::Error for Error {
 impl fmt::Display for Error {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
+			Error::IoError(error) => fmt::Display::fmt(error, f),
+            Error::LogicError(message) => fmt::Display::fmt(message, f),
+			Error::SyntaxError(message) => fmt::Display::fmt(message, f),
 			Error::XmlError(error) => fmt::Display::fmt(error, f),
-			Error::IoError(error) => fmt::Display::fmt(error, f), 
-			Error::SyntaxError(message) => fmt::Display::fmt(message, f)
 		}
 	}
 }
