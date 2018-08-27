074b6c6d5a005b9e727a646b50a52912acd90161
jayrave <jayanthan.raveendiran@gmail.com>
2018-08-27 00:51:48 -0700

    refactor `xml_reader` to use `Rc` & `RefCell`

--- src/reader/xml_reader.rs
+++ src/reader/xml_reader.rs
@@ -2,8 +2,11 @@ use android_string::AndroidString;
 use reader::error::Error;
 use std::io::BufReader;
 use std::io::Read;
+use std::rc::Rc;
+use std::cell::RefCell;
+use xml::attribute::OwnedAttribute;
 use xml::ParserConfig;
-use xml::reader::{EventReader, XmlEvent};
+use xml::reader::{XmlEvent};
 
 const RESOURCES_EVENT_DEPTH: u32 = 0;
 const STRING_EVENT_DEPTH: u32 = 1;
@@ -17,81 +20,186 @@ const TRUE_TEXT: &str = "true";
 const FALSE_TEXT: &str = "false";
 
 pub fn from<R : Read>(read: R) -> Result<Vec<AndroidString>, Error> {
-	let mut android_strings = vec!();
+	let mut events_handler = EventsHandler::new();
 	let reader = ParserConfig::new()
 		.cdata_to_characters(true)
 		.create_reader(BufReader::new(read));
 
-	let mut depth = 0u32;
-	let mut is_inside_resources_event = false;
-	let mut is_inside_string_event = false;
-
-	let mut string_name: Option<String> = None;
-	let mut is_translatable_string = false;
-
-	for element in reader {
-		match element {
+	for element_or_error in reader {
+		match element_or_error {
 			Err(error) => return Err(Error::XmlError(error)),
 			Ok(element) => match element {
-				XmlEvent::StartElement { name, attributes, .. } => match name.local_name.as_str() {
-					RESOURCES_TEXT => if depth == RESOURCES_EVENT_DEPTH {
-						depth += 1;
-						is_inside_resources_event = true;
-					}, 
-
-					STRING_TEXT => if depth == STRING_EVENT_DEPTH && is_inside_resources_event {
-						depth += 1;
-						is_inside_string_event = true;
-						
-						string_name = None;
-						is_translatable_string = true;
-						for attribute in attributes {
-							match attribute.name.local_name.as_str() {
-								STRING_NAME_TEXT => string_name = Some(attribute.value),
-								STRING_TRANSLATABLE_TEXT => match attribute.value.as_str() {
-									TRUE_TEXT => is_translatable_string = true, 
-									FALSE_TEXT => is_translatable_string = false, 
-									_ => {}
-								}, 
-								_ => {}
-							}
-						}
-					}, 
-
-					_ => depth += 1
-				},
-
-				XmlEvent::EndElement { name } => match name.local_name.as_str() {
-					RESOURCES_TEXT => if depth == RESOURCES_EVENT_DEPTH + 1 && is_inside_resources_event {
-						depth -= 1;
-						is_inside_resources_event = false;
-					}, 
-
-					STRING_TEXT => if depth == STRING_EVENT_DEPTH + 1 && is_inside_string_event {
-						depth -= 1;
-						is_inside_string_event = false;
-					}, 
-
-					_ => depth -= 1
-				},
-
-				XmlEvent::Characters(c) => if depth == CHARACTER_EVENT_DEPTH && is_inside_string_event {
-					match string_name {
-						None => return Err(Error::SyntaxError(format!("Name is missing for text \"{}\"", &c))), 
-						Some(ref name) => android_strings.push(AndroidString::new(
-							String::from(name.as_str()),
-							c, 
-							is_translatable_string
-						))
-					}
-				}
-
-				_ => {}
+				XmlEvent::StartElement { name, attributes, .. } => events_handler.handle_start_element_event(name.local_name, attributes)?,
+				XmlEvent::Characters(text) => events_handler.handle_characters_event(text),
+				XmlEvent::EndElement { .. } => events_handler.handle_end_element_event(),
+				_ => {} // No op for other events
 			}
 		}
 	}
 
-	Ok(android_strings)
+	events_handler.strings()
+}
+
+struct EventsHandler {
+	android_strings: Rc<RefCell<Vec<AndroidString>>>,
+	event_handlers: Vec<Box<EventHandler>>
+}
+
+impl EventsHandler {
+	fn new() -> EventsHandler {
+        let strings = Rc::new(RefCell::new(vec!()));
+        let strings_clone = Rc::clone(&strings);
+		EventsHandler {
+			android_strings: strings,
+			event_handlers: vec!(Box::new(RootEventHandler::new(strings_clone)))
+		}
+	}
+
+	fn handle_start_element_event(&mut self, tag_name: String, attributes: Vec<OwnedAttribute>) -> Result<(), Error> {
+		let event_handler = self.event_handlers.last_mut().unwrap().handler_for_start_element_event(tag_name, attributes)?;
+		self.event_handlers.push(event_handler);
+		Ok(())
+	}
+
+	fn handle_end_element_event(&mut self) {
+		self.event_handlers.pop();
+	}
+
+	fn handle_characters_event(&mut self, text: String) {
+		self.event_handlers.last_mut().unwrap().handle_characters_event(text);
+	}
+
+    fn strings(mut self) -> Result<Vec<AndroidString>, Error> {
+        self.event_handlers.clear();
+        match Rc::try_unwrap(self.android_strings) {
+            Err(ref_cell) => Err(Error::LogicError(format!("Rc has {} strong references!", Rc::strong_count(&ref_cell)))),
+            Ok(ref_cell) => Ok(ref_cell.into_inner())
+        }
+    }
+}
+
+trait EventHandler {
+	fn handler_for_start_element_event(&self, tag_name: String, attributes: Vec<OwnedAttribute>) -> Result<Box<EventHandler>, Error>;
+	fn handle_characters_event(&self, text: String);
+}
+
+struct RootEventHandler {
+	strings: Rc<RefCell<Vec<AndroidString>>>
+}
+
+impl RootEventHandler {
+	fn new(strings: Rc<RefCell<Vec<AndroidString>>>) -> RootEventHandler {
+		RootEventHandler { 
+			strings
+		}
+	}
+}
+
+impl EventHandler for RootEventHandler {
+	fn handler_for_start_element_event(&self, tag_name: String, _attributes: Vec<OwnedAttribute>) -> Result<Box<EventHandler>, Error> {
+		match tag_name.as_str() {
+			RESOURCES_TEXT => Ok(Box::new(ResourcesEventHandler::new(Rc::clone(&self.strings)))),
+			_ => Ok(Box::new(SinkingEventHandler::new()))
+		}
+	}
+
+	fn handle_characters_event(&self, _text: String) {
+        // No op
+	}
+}
+
+struct ResourcesEventHandler {
+	strings: Rc<RefCell<Vec<AndroidString>>>
+}
+
+impl ResourcesEventHandler {
+	fn new(strings: Rc<RefCell<Vec<AndroidString>>>) -> ResourcesEventHandler {
+		ResourcesEventHandler {
+			strings
+		}
+	}
+}
+
+impl EventHandler for ResourcesEventHandler {
+	fn handler_for_start_element_event(&self, tag_name: String, attributes: Vec<OwnedAttribute>) -> Result<Box<EventHandler>, Error> {
+		match tag_name.as_str() {
+			STRING_TEXT => Ok(Box::new(StringEventHandler::new(Rc::clone(&self.strings), attributes)?)),
+			_ => Ok(Box::new(SinkingEventHandler::new()))
+		}
+	}
+
+	fn handle_characters_event(&self, _text: String) {
+        // No op
+	}
+}
+
+struct StringEventHandler {
+	strings: Rc<RefCell<Vec<AndroidString>>>,
+	name: String,
+	is_translatable: bool
+}
+
+impl StringEventHandler {
+	fn new(strings: Rc<RefCell<Vec<AndroidString>>>, attributes: Vec<OwnedAttribute>) -> Result<StringEventHandler, Error> {
+		let mut string_name = None;
+		let mut is_translatable = true;
+		for attribute in attributes {
+			match attribute.name.local_name.as_str() {
+				STRING_NAME_TEXT => string_name = Some(attribute.value),
+				STRING_TRANSLATABLE_TEXT => match attribute.value.as_str() {
+					TRUE_TEXT => is_translatable = true, 
+					FALSE_TEXT => is_translatable = false, 
+					_ => {}
+				}, 
+				_ => {}
+			}
+		}
+
+		match string_name {
+			None => Err(Error::SyntaxError(String::from("string element is missing required name attribute"))),
+			Some(name) => Ok(StringEventHandler {
+				strings,
+				name,
+				is_translatable
+			})
+		}
+	}
+}
+
+impl EventHandler for StringEventHandler {
+	fn handler_for_start_element_event(&self, _tag_name: String, _attributes: Vec<OwnedAttribute>) -> Result<Box<EventHandler>, Error> {
+		Ok(Box::new(SinkingEventHandler::new()))
+	}
+
+	fn handle_characters_event(&self, text: String) {
+		self.strings.borrow_mut().push(AndroidString::new(
+			self.name.clone(),
+			text, 
+			self.is_translatable
+		));
+	}
+}
+
+struct SinkingEventHandler {
+
+}
+
+impl SinkingEventHandler {
+	fn new() -> SinkingEventHandler {
+		SinkingEventHandler {
+
+		}
+	}
+}
+
+impl EventHandler for SinkingEventHandler {
+	fn handler_for_start_element_event(&self, _tag_name: String, _attributes: Vec<OwnedAttribute>) -> Result<Box<EventHandler>, Error> {
+		Ok(Box::new(SinkingEventHandler::new()))
+	}
+
+	fn handle_characters_event(&self, _text: String) {
+        // No op
+	}
 }
 
 #[cfg(test)]
@@ -100,7 +208,7 @@ mod tests {
 
 	use ::android_string::AndroidString;
 	use std::fs::File;
-	use std::io::{Write, Read, Seek, SeekFrom};
+	use std::io::{Write, Seek, SeekFrom};
 
 	#[test]
 	fn strings_are_read_from_valid_clean_file() {
