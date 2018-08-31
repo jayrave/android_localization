66db237e15fd4e30717538767496da8b1d834959
jayrave <jayanthan.raveendiran@gmail.com>
2018-08-31 00:24:55 -0700

    add `dedup`

--- src/android_string.rs
+++ src/android_string.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct AndroidString {
     name: String,
     value: String,
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate xml;
 
 mod android_string;
 mod constants;
+mod ops;
 mod reader;
 mod translatable_android_string;
 mod writer;
