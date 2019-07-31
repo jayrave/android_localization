bf7a676e634130d5db2eda407f0104d3c44b289c
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2019-07-31 01:27:30 -0700

    refactor MultiLocaleWriter

--- core/src/writer/csv_writer/multi_locale_writer.rs
+++ core/src/writer/csv_writer/multi_locale_writer.rs
@@ -1,4 +1,4 @@
-use csv::Writer;
+use csv;
 use localizable_strings::LocalizableStrings;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
@@ -7,10 +7,10 @@ use std::hash::Hasher;
 use std::io::Write;
 use writer::csv_writer::error::Error;
 
-pub fn write<W: Write>(
+pub fn write(
     strings_list: Vec<LocalizableStrings>,
-    sink_provider: &mut SinkProvider<W>,
-) -> Result<Vec<Box<W>>, Error> {
+    sink_provider: &mut SinkProvider,
+) -> Result<(), Error> {
     // Split strings into groups requiring localization for the same strings
     let mut grouped_strings_list: HashMap<u64, Vec<LocalizableStrings>> = HashMap::new();
     for strings in strings_list {
@@ -22,51 +22,16 @@ pub fn write<W: Write>(
 
     // We may need multiple sinks to write locale requiring
     // different strings to be localized
-    let mut all_sinks = Vec::with_capacity(grouped_strings_list.len());
     for (_, some_strings_list) in grouped_strings_list.into_iter() {
-        let mut sink = sink_provider.new_sink(
-            some_strings_list
-                .iter()
-                .map(|s| String::from(s.to_locale()))
-                .collect(),
-        );
+        let for_locales = some_strings_list
+            .iter()
+            .map(|s| String::from(s.to_locale()))
+            .collect();
 
-        write_to_sink(some_strings_list, &mut sink)?;
-        all_sinks.push(sink);
+        let writer = Writer { strings_list: some_strings_list };
+        sink_provider.execute_with_new_sink(for_locales, writer)?;
     }
 
-    Ok(all_sinks)
-}
-
-pub fn write_to_sink<W: Write>(
-    strings_list: Vec<LocalizableStrings>,
-    sink: &mut W,
-) -> Result<(), Error> {
-    // Sink is automatically buffered
-    let mut writer = Writer::from_writer(sink);
-    let locale_count = strings_list.len();
-    let localizable_strings = strings_list.first().unwrap();
-    let value_count = localizable_strings.default_locale_strings().len();
-
-    // Write header record
-    let mut header = Vec::with_capacity(locale_count + 2);
-    header.push("string_name");
-    header.push("default_locale");
-    for i in 0..locale_count {
-        header.push(strings_list[i].to_locale());
-    }
-    writer.write_record(header)?;
-
-    // Write values
-    let mut record = vec![""; locale_count + 2];
-    for i in 0..value_count {
-        let localizable_string = localizable_strings.default_locale_strings().get(i).unwrap();
-        record[0] = localizable_string.name();
-        record[1] = localizable_string.value();
-        writer.write_record(&record)?;
-    }
-
-    writer.flush()?;
     Ok(())
 }
 
@@ -76,24 +41,70 @@ fn find_grouping_hash_of(strings: &LocalizableStrings) -> u64 {
     hasher.finish()
 }
 
-pub trait SinkProvider<W: Write> {
-    fn new_sink(&mut self, for_locales: Vec<String>) -> Box<W>;
+struct Writer {
+    strings_list: Vec<LocalizableStrings>
+}
+
+impl Writer {
+    fn write(self, sink: &mut Write) -> Result<(), Error> {
+        // Sink is automatically buffered
+        let mut csv_writer = csv::Writer::from_writer(sink);
+        let locale_count = self.strings_list.len();
+        let localizable_strings = self.strings_list.first().unwrap();
+        let value_count = localizable_strings.default_locale_strings().len();
+
+        // Write header record
+        let mut header = Vec::with_capacity(locale_count + 2);
+        header.push("string_name");
+        header.push("default_locale");
+        for i in 0..locale_count {
+            header.push(self.strings_list[i].to_locale());
+        }
+        csv_writer.write_record(header)?;
+
+        // Write values
+        let mut record = vec![""; locale_count + 2];
+        for i in 0..value_count {
+            let localizable_string = localizable_strings.default_locale_strings().get(i).unwrap();
+            record[0] = localizable_string.name();
+            record[1] = localizable_string.value();
+            csv_writer.write_record(&record)?;
+        }
+
+        csv_writer.flush()?;
+        Ok(())
+    }
+}
+
+pub trait SinkProvider {
+    fn execute_with_new_sink(
+        &mut self,
+        for_locales: Vec<String>,
+        writer: Writer
+    ) -> Result<(), Error>;
 }
 
 #[cfg(test)]
 mod tests {
+    use super::Error;
     use super::SinkProvider;
     use android_string::AndroidString;
     use localizable_strings::LocalizableStrings;
+    use std::cell::RefCell;
+    use super::Writer;
+    use std::io::Write;
 
     struct ByteSinkProvider {
-        for_locales_list: Vec<Vec<String>>,
+        data: Vec<(Vec<String>, String)>
     }
 
-    impl SinkProvider<Vec<u8>> for ByteSinkProvider {
-        fn new_sink(&mut self, for_locales: Vec<String>) -> Box<Vec<u8>> {
-            self.for_locales_list.push(for_locales);
-            Box::new(vec![])
+    impl SinkProvider for ByteSinkProvider {
+        fn execute_with_new_sink(&mut self, for_locales: Vec<String>, writer: Writer) -> Result<(), Error> {
+            let mut contents = vec![];
+            let result = writer.write(&mut contents);
+            self.data.push((for_locales, String::from_utf8(contents).unwrap()));
+
+            result
         }
     }
 
@@ -136,35 +147,21 @@ mod tests {
 
         // Convert all the written bytes into strings for the different sinks
         let mut sink_provider = ByteSinkProvider {
-            for_locales_list: vec![],
+            data: vec![],
         };
 
-        let mut written_contents = super::write(strings_list, &mut sink_provider)
-            .unwrap()
-            .into_iter()
-            .map(|s| String::from_utf8(s.to_vec()).unwrap())
-            .collect::<Vec<String>>();
+        super::write(strings_list, &mut sink_provider);
 
         // Since a map is used, sort the contents to be sure of the order
-        written_contents.sort();
-        sink_provider.for_locales_list.sort();
+        sink_provider.data.sort();
 
         // Time to assert
         assert_eq!(
-            written_contents,
+            sink_provider.data,
             vec![
-                String::from("string_name,default_locale,french\nstring_1,english 1,\n"),
-                String::from("string_name,default_locale,german,dutch\nstring_1,english 1,,\nstring_2,english 2,,\n"),
-                String::from("string_name,default_locale,spanish\nstring_2,english 2,\n")
-            ]
-        );
-
-        assert_eq!(
-            sink_provider.for_locales_list,
-            vec![
-                vec![String::from("french")],
-                vec![String::from("german"), String::from("dutch")],
-                vec![String::from("spanish")]
+                (vec![String::from("french")], String::from("string_name,default_locale,french\nstring_1,english 1,\n")),
+                (vec![String::from("german"), String::from("dutch")], String::from("string_name,default_locale,german,dutch\nstring_1,english 1,,\nstring_2,english 2,,\n")),
+                (vec![String::from("spanish")], String::from("string_name,default_locale,spanish\nstring_2,english 2,\n"))
             ]
         );
     }
