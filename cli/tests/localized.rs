112de4ebe3d8bb0a62400facad64b4c66857aee2
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2019-08-11 20:20:47 -0700

    add integration tests for localized to xml

---
+++ cli/tests/localized.rs
@@ -0,0 +1,131 @@
+mod helpers;
+
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use tempfile::TempDir;
+
+#[test]
+fn one_locale_per_file_with_mapping() {
+    execute_with_copied_sample_res(tempfile::tempdir().unwrap(), |output_res_path: String| {
+        android_localization_cli::do_the_thing(vec![
+            "does_not_matter",
+            "localized",
+            "--res",
+            &output_res_path.clone(),
+            "--input",
+            "./tests_data/localized/input/one_locale_per_file_with_mapping",
+            "--mapping",
+            "french=fr",
+            "--mapping",
+            "spanish=es",
+        ])
+        .unwrap();
+
+        helpers::assert_equality_of_file_contents(
+            "./tests_data/localized/output/",
+            "french_strings.xml",
+            &format!("{}/values-fr/", output_res_path),
+            "strings.xml",
+        );
+        helpers::assert_equality_of_file_contents(
+            "./tests_data/localized/output/",
+            "spanish_strings.xml",
+            &format!("{}/values-es/", output_res_path),
+            "strings.xml",
+        );
+    })
+}
+
+#[test]
+fn one_locale_per_file_without_mapping() {
+    execute_with_copied_sample_res(tempfile::tempdir().unwrap(), |output_res_path: String| {
+        android_localization_cli::do_the_thing(vec![
+            "does_not_matter",
+            "localized",
+            "--res",
+            &output_res_path.clone(),
+            "--input",
+            "./tests_data/localized/input/one_locale_per_file_without_mapping",
+        ])
+        .unwrap();
+
+        helpers::assert_equality_of_file_contents(
+            "./tests_data/localized/output/",
+            "french_strings.xml",
+            &format!("{}/values-fr/", output_res_path),
+            "strings.xml",
+        );
+        helpers::assert_equality_of_file_contents(
+            "./tests_data/localized/output/",
+            "spanish_strings.xml",
+            &format!("{}/values-es/", output_res_path),
+            "strings.xml",
+        );
+    })
+}
+
+fn execute_with_copied_sample_res<F>(temp_dir: TempDir, test: F)
+where
+    F: FnOnce(String) -> (),
+{
+    // Build paths
+    let mut res_dir_path = temp_dir.path().to_path_buf();
+    res_dir_path.push("res");
+
+    let mut default_values_dir_path = res_dir_path.clone();
+    default_values_dir_path.push("values");
+    let mut default_strings_file_path = default_values_dir_path.clone();
+    default_strings_file_path.push("strings.xml");
+
+    let mut fr_values_dir_path = res_dir_path.clone();
+    fr_values_dir_path.push("values-fr");
+    let mut fr_strings_file_path = fr_values_dir_path.clone();
+    fr_strings_file_path.push("strings.xml");
+
+    let mut es_values_dir_path = res_dir_path.clone();
+    es_values_dir_path.push("values-es");
+    let mut es_strings_file_path = es_values_dir_path.clone();
+    es_strings_file_path.push("strings.xml");
+
+    // Create required dirs & files
+    fs::create_dir_all(default_values_dir_path.clone()).unwrap();
+    fs::create_dir_all(fr_values_dir_path.clone()).unwrap();
+    fs::create_dir_all(es_values_dir_path.clone()).unwrap();
+    let mut default_strings_file = File::create(default_strings_file_path.clone()).unwrap();
+    let mut fr_strings_file = File::create(fr_strings_file_path.clone()).unwrap();
+    let mut es_strings_file = File::create(es_strings_file_path.clone()).unwrap();
+
+    // Write out required contents into files
+    default_strings_file
+        .write(
+            helpers::read_file_contents(
+                "./tests_data/localized/input/sample_res/values/",
+                "strings.xml",
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+
+    fr_strings_file
+        .write(
+            helpers::read_file_contents(
+                "./tests_data/localized/input/sample_res/values-fr/",
+                "strings.xml",
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+
+    es_strings_file
+        .write(
+            helpers::read_file_contents(
+                "./tests_data/localized/input/sample_res/values-es/",
+                "strings.xml",
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+
+    test(String::from(res_dir_path.to_str().unwrap()))
+}
