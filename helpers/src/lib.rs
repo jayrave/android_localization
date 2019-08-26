50f8a1b131d4600d9967c49955ea63437686bf99
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2019-08-26 01:12:56 -0700

    add `helpers` module

---
+++ helpers/src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+
+/// To easily add context to panic messages
+pub trait DevExpt<T, S: AsRef<str>> {
+    fn expt(self, msg: S) -> T;
+}
+
+impl<T, S: AsRef<str>> DevExpt<T, S> for Option<T> {
+    fn expt(self, msg: S) -> T {
+        self.expect(&build_message_to_contact_dev(msg))
+    }
+}
+
+impl<T, S: AsRef<str>, E: Error> DevExpt<T, S> for Result<T, E> {
+    fn expt(self, msg: S) -> T {
+        self.expect(&build_message_to_contact_dev(msg))
+    }
+}
+
+fn build_message_to_contact_dev<T: AsRef<str>>(msg: T) -> String {
+    format!(
+        r#"{}
+This is not supposed to happen!
+Please file an issue at https://github.com/jayrave/android_localization"#,
+        msg.as_ref()
+    )
+}
