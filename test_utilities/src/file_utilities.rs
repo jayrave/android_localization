db7487397d42fb5b9b13d8178b465e042e976643
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2019-09-02 23:03:36 -0400

    add test utilities for file & res

---
+++ test_utilities/src/file_utilities.rs
@@ -0,0 +1,18 @@
+use std::fmt::Debug;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+pub fn read_content<P: AsRef<Path>>(file_path: P) -> String {
+    let mut content = String::new();
+    let mut file = File::open(file_path).unwrap();
+    file.read_to_string(&mut content);
+    content
+}
+
+pub fn write_content<P: AsRef<Path>, S: Into<String>>(file_path: P, content: S) {
+    let mut file = File::create(file_path).unwrap();
+    file.write(content.into().as_bytes()).unwrap();
+}
--- test_utilities/src/lib.rs
+++ test_utilities/src/lib.rs
@@ -4,4 +4,6 @@ pub use list_assertions::assert_list_is_empty;
 pub use list_assertions::assert_strict_list_eq;
 
 mod equality_assertions;
+pub mod file_utilities;
 mod list_assertions;
+pub mod res_utilities;
