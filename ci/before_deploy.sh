48b1893b7e340e70d110b2ca2041ada49f8086d7
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2019-09-17 23:42:07 -0700

    host gzipped executables in github releases

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,4 +1,10 @@
 set -ex
 
+# Build the required executable
 cargo build --all --release --target $TARGET --verbose
-cp target/$TARGET/release/android_localization_cli android_localization-$TRAVIS_TAG-$TARGET
+
+# Give a more manageable name for the executable
+cp target/$TARGET/release/android_localization_cli android_localization
+
+# Zip up the executable with name that resembles android_localization-0.1.4-x86_64-unknown-linux-gnu
+tar czf android_localization-$TRAVIS_TAG-$TARGET.tar.gz android_localization
