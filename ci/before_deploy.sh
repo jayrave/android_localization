757910473c872783aedb99d4b02ba187bced1e84
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2019-09-22 12:40:30 -0700

    make sure to carry along the `.exe` extension for windows

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,8 +3,8 @@ set -ex
 # Build the required executable
 cargo build --all --release --target $TARGET --verbose
 
-# Give a more manageable name for the executable
-cp target/$TARGET/release/android_localization_cli android_localization
+# Rename the executable
+cp target/$TARGET/release/android_localization_cli$EXE_SUFFIX android_localization$EXE_SUFFIX
 
 # Zip up the executable with name that resembles android_localization-0.1.4-x86_64-unknown-linux-gnu
-tar czf android_localization-$TRAVIS_TAG-$TARGET.tar.gz android_localization
+tar czf android_localization-$TRAVIS_TAG-$TARGET.tar.gz android_localization$EXE_SUFFIX
