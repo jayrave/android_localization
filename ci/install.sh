e885a2a16d7e768dd2a612154078502cdfb0695b
jayrave <jayanthan.raveendiran@gmail.com>
2020-02-13 01:19:32 -0800

    fix toolchain issue in `ci/install.sh`

--- ci/install.sh
+++ ci/install.sh
@@ -2,5 +2,5 @@ set -ex
 
 # Install required rust components
 rustup target add $TARGET
-rustup component add rustfmt --toolchain $TARGET
-rustup component add clippy --toolchain $TARGET
+rustup component add rustfmt
+rustup component add clippy
